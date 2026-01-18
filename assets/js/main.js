16c647ae14aa4574afbe062fff4d4ee0c89fbc76
Mohammed-6002 <302352550@student.rocmondriaan.nl>
2026-01-18 18:49:28 +0100

    save

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+(function(){
+  const path = (location.pathname.split('/').pop() || 'index.html').toLowerCase();
+
+  document.querySelectorAll('[data-nav] a.nav-link').forEach(a => {
+    const href = (a.getAttribute('href') || '').toLowerCase();
+    const target = href.split('/').pop();
+    if (target === path) {
+      a.classList.add('active');
+      a.setAttribute('aria-current', 'page');
+    }
+  });
+})();
